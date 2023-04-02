1e720174d7bedc5da9c20bec9d77c9ee35baeaf7
VanDung <vandung030701@gmail.com>
2023-04-02 10:41:32 +0700

    Dung commit

--- MomShopFE/src/app/weather/weather.component.ts
+++ MomShopFE/src/app/weather/weather.component.ts
@@ -8,11 +8,78 @@ import { HttpClient } from '@angular/common/http';
 })
 export class WeatherComponent implements OnInit {
   weatherData: any;
-
+ tableData;
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getWeatherData();
+    this.tableData = [
+      {
+          firstname: 'David',
+          lastname: 'ace',
+          age: '40',
+      },
+      {
+          firstname: 'AJne',
+          lastname: 'west',
+          age: '40',
+      },
+      {
+          firstname: 'Mak',
+          lastname: 'Lame',
+          age: '40',
+      },
+      {
+          firstname: 'Peter',
+          lastname: 'raw',
+          age: '40',
+      },
+      {
+          firstname: 'Kane',
+          lastname: 'James',
+          age: '40',
+      },
+      {
+          firstname: 'Peter',
+          lastname: 'raw',
+          age: '40',
+      },
+      {
+          firstname: 'Kane',
+          lastname: 'James',
+          age: '40',
+      },
+      {
+          firstname: 'Peter',
+          lastname: 'raw',
+          age: '40',
+      },
+      {
+          firstname: 'Kane',
+          lastname: 'James',
+          age: '40',
+      },
+      {
+          firstname: 'Peter',
+          lastname: 'raw',
+          age: '40',
+      },
+      {
+          firstname: 'Kane',
+          lastname: 'James',
+          age: '40',
+      },
+      {
+          firstname: 'Peter',
+          lastname: 'raw',
+          age: '40',
+      },
+      {
+          firstname: 'Kane',
+          lastname: 'James',
+          age: '40',
+      },
+  ];
   }
 
   getWeatherData(): void {
