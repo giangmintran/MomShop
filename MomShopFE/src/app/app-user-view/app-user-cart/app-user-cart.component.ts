8b7843181aee6654a0c13e45b1fbec4778099c12
VanDung <vandung030701@gmail.com>
2023-05-03 19:25:46 +0700

    Dung commit

--- MomShopFE/src/app/app-user-view/app-user-cart/app-user-cart.component.ts
+++ MomShopFE/src/app/app-user-view/app-user-cart/app-user-cart.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-user-cart',
-  templateUrl: './app-user-cart.component.html',
-  styleUrls: ['./app-user-cart.component.scss']
+  selector: "app-user-cart",
+  templateUrl: "./app-user-cart.component.html",
+  styleUrls: ["./app-user-cart.component.scss"],
 })
 export class AppUserCartComponent {
   cart;
+  quantityProduct = 1;
+  minusQuantityProduct() {
+    if(this.quantityProduct === 1){
+      return;
+    }
+    var quantity = document.getElementsByClassName("text-quantity")[0];
+    this.quantityProduct--;
+    quantity.textContent = this.quantityProduct.toString();
+  }
+  addQuantityProduct() {
+    var quantity = document.getElementsByClassName("text-quantity")[0];
+    this.quantityProduct++;
+    quantity.textContent = this.quantityProduct.toString();
+  }
+  deleteProduct(){
+    document.getElementsByClassName("fa-trash")[0].parentElement.parentElement.remove();
+  }
 }
