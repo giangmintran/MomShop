bea1436ca8de443a4e5bcaca1a089aefaafc1e63
dungdv <vandung030701@gmail.com>
2023-05-08 18:00:44 +0700

    Dung commit

--- MomShopFE/src/app/app-user-view/app-user-home/app-user-home.component.ts
+++ MomShopFE/src/app/app-user-view/app-user-home/app-user-home.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app-user-home.component.scss']
 })
 export class AppUserHomeComponent {
-
+  count = 0;
+  constructor() {
+  }
+  translate(option) {
+    var blockProduct = document.querySelector<HTMLElement>(".block-product");
+    var offsetY = blockProduct.getBoundingClientRect();
+    var itemBlock = document.querySelectorAll<HTMLElement>(".item-product");
+    if (option == 1) {
+      console.log('test');
+      this.count += 1;
+      for (let i = 0; i < itemBlock.length; i++) {
+        var offsetItem = itemBlock[i].getBoundingClientRect();
+        if (offsetItem.top <= offsetY.bottom) {
+          itemBlock[i].style.transform = `translateX(-${(blockProduct.offsetWidth / 3) * this.count}px)`
+        }
+      }
+    }
+  }
 }
