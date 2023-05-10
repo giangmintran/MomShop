ce2457c5b42baa1d5ec19515a7d79ee7f0419fa6
dungdv <vandung030701@gmail.com>
2023-05-10 17:11:49 +0700

    Dung commit check-out

--- MomShopFE/src/app/app-user-view/check-out/check-out.component.ts
+++ MomShopFE/src/app/app-user-view/check-out/check-out.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-check-out',
@@ -8,9 +9,25 @@ import { Component } from '@angular/core';
 })
 export class CheckOutComponent {
   data;
-  constructor(private http:HttpClient) {
-    this.http.get("https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/province").subscribe(data=>{
-          console.log(data);
+  saleOffs: string;
+  constructor(private http: HttpClient, private router: Router) {
+    this.http.get("https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/province").subscribe(data => {
+      console.log(data);
     })
+  }
+  login() {
+    this.router.navigateByUrl('/login')
+  }
+  saleOff() {
+    var button = document.querySelector<HTMLElement>(".buttton-use");
+    var input = document.querySelector<HTMLElement>(".input-sale-off");
+    if (this.saleOffs.trim() !== "") {
+      button.style.backgroundColor = "#338dbc";
+      button.setAttribute('disabled', 'false');
+    }
+    else{
+      button.style.backgroundColor = "#c8c8c8";
+      button.setAttribute('disabled', 'true');
+    }
   }
 }
