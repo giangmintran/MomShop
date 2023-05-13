d726f36650fedd8af0b5c6230eab9dbddce2b955
VanDung <vandung030701@gmail.com>
2023-05-13 14:11:53 +0700

    Dung commit nav

--- MomShopFE/src/app/app-user-view/app-header/app-header.component.ts
+++ MomShopFE/src/app/app-user-view/app-header/app-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: "app-header",
@@ -10,11 +11,15 @@ export class AppHeaderComponent {
   @Output() modalSaves: EventEmitter<any> = new EventEmitter<any>();
   @Output() backHome: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private toastr: ToastrService) {}
   routerToCart() {
     this.modalSaves.emit(null);
   }
   backToHome() {
     this.backHome.emit(null);
+  }
+  logout() {
+    this.toastr.success("Đăng xuất thành công", "Thông báo", { timeOut: 2000 });
+    this.router.navigateByUrl('/home');
   }
 }
